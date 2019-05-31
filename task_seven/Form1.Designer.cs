33670714153658030b98c20bf1f4b0c6e96bea89
stayl <staylimmk@gmail.com>
2019-05-31 16:57:42 +0500

    Добавьте файлы проекта.

---
+++ task_seven/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace task_seven
+{
+    partial class Bul_Function
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.func = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.answ = new System.Windows.Forms.Label();
+            this.zero = new System.Windows.Forms.Button();
+            this.one = new System.Windows.Forms.Button();
+            this.star = new System.Windows.Forms.Button();
+            this.calculate = new System.Windows.Forms.Button();
+            this.answer = new System.Windows.Forms.Label();
+            this.delete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(63, 41);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(202, 36);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyUp);
+            // 
+            // func
+            // 
+            this.func.AutoSize = true;
+            this.func.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.func.Location = new System.Drawing.Point(15, 46);
+            this.func.Name = "func";
+            this.func.Size = new System.Drawing.Size(46, 25);
+            this.func.TabIndex = 1;
+            this.func.Text = "F = ";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(321, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(366, 365);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // answ
+            // 
+            this.answ.AutoSize = true;
+            this.answ.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.answ.Location = new System.Drawing.Point(15, 238);
+            this.answ.Name = "answ";
+            this.answ.Size = new System.Drawing.Size(78, 25);
+            this.answ.TabIndex = 3;
+            this.answ.Text = "Ответ:";
+            // 
+            // zero
+            // 
+            this.zero.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.zero.Location = new System.Drawing.Point(63, 99);
+            this.zero.Name = "zero";
+            this.zero.Size = new System.Drawing.Size(45, 43);
+            this.zero.TabIndex = 4;
+            this.zero.Text = "0";
+            this.zero.UseVisualStyleBackColor = true;
+            this.zero.Click += new System.EventHandler(this.zero_Click);
+            // 
+            // one
+            // 
+            this.one.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.one.Location = new System.Drawing.Point(114, 99);
+            this.one.Name = "one";
+            this.one.Size = new System.Drawing.Size(45, 43);
+            this.one.TabIndex = 5;
+            this.one.Text = "1";
+            this.one.UseVisualStyleBackColor = true;
+            this.one.Click += new System.EventHandler(this.one_Click);
+            // 
+            // star
+            // 
+            this.star.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.star.Location = new System.Drawing.Point(169, 99);
+            this.star.Name = "star";
+            this.star.Size = new System.Drawing.Size(45, 43);
+            this.star.TabIndex = 6;
+            this.star.Text = "*";
+            this.star.UseVisualStyleBackColor = true;
+            this.star.Click += new System.EventHandler(this.star_Click);
+            // 
+            // calculate
+            // 
+            this.calculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.calculate.Location = new System.Drawing.Point(65, 153);
+            this.calculate.Name = "calculate";
+            this.calculate.Size = new System.Drawing.Size(200, 47);
+            this.calculate.TabIndex = 7;
+            this.calculate.Text = "Вычислить";
+            this.calculate.UseVisualStyleBackColor = true;
+            this.calculate.Click += new System.EventHandler(this.calculate_Click);
+            // 
+            // answer
+            // 
+            this.answer.AutoSize = true;
+            this.answer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.answer.Location = new System.Drawing.Point(99, 238);
+            this.answer.Name = "answer";
+            this.answer.Size = new System.Drawing.Size(0, 25);
+            this.answer.TabIndex = 8;
+            // 
+            // delete
+            // 
+            this.delete.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.delete.Location = new System.Drawing.Point(220, 99);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(45, 43);
+            this.delete.TabIndex = 9;
+            this.delete.Text = "Del";
+            this.delete.UseVisualStyleBackColor = true;
+            this.delete.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Bul_Function
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(709, 446);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.answer);
+            this.Controls.Add(this.calculate);
+            this.Controls.Add(this.star);
+            this.Controls.Add(this.one);
+            this.Controls.Add(this.zero);
+            this.Controls.Add(this.answ);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.func);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Bul_Function";
+            this.Text = "Определитель монотонности";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label func;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label answ;
+        private System.Windows.Forms.Button zero;
+        private System.Windows.Forms.Button one;
+        private System.Windows.Forms.Button star;
+        private System.Windows.Forms.Button calculate;
+        private System.Windows.Forms.Label answer;
+        private System.Windows.Forms.Button delete;
+    }
+}
+
---
+++ task_seven/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task_seven
+{
+    public partial class Bul_Function : Form
+    {
+        static int[,] matrix;
+        static int starcount = 0;
+
+        public Bul_Function()
+        {
+            InitializeComponent();
+            calculate.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void zero_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 0;
+            if (Convert.ToInt32(Math.Log(textBox1.Text.Length, 2)) == Math.Log(textBox1.Text.Length, 2) && textBox1.Text.Length != 1) calculate.Enabled = true;
+            else calculate.Enabled = false;
+        }
+
+        private void one_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 1;
+            if (Convert.ToInt32(Math.Log(textBox1.Text.Length, 2)) == Math.Log(textBox1.Text.Length, 2) && textBox1.Text.Length != 1) calculate.Enabled = true;
+            else calculate.Enabled = false;
+        }
+
+        private void star_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "*";
+            starcount++;
+            if (Convert.ToInt32(Math.Log(textBox1.Text.Length, 2)) == Math.Log(textBox1.Text.Length, 2) && textBox1.Text.Length != 1) calculate.Enabled = true;
+            else calculate.Enabled = false;
+            //if (starcount >= Math.Log(textBox1.Text.Length, 2)) star.Enabled = false;
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == '0' || e.KeyChar == '1' || e.KeyChar == '*' || e.KeyChar == '\b') e.Handled = false;
+            else e.Handled = true;
+        }
+
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (textBox1.Text.Length > 0)
+                if (Convert.ToInt32(Math.Log(textBox1.Text.Length, 2)) == Math.Log(textBox1.Text.Length, 2) && textBox1.Text.Length != 1) calculate.Enabled = true;
+                else calculate.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            starcount = 0;
+            answer.Text = "";
+        }
+
+        private void calculate_Click(object sender, EventArgs e)
+        {
+            int[,] starset = BoolSet((int)Math.Pow(2, starcount));
+            int[] position = new int[starcount];
+            int pos = 0;
+            // remember the positions of stars
+            for (int index = 0; index < textBox1.Text.Length; index++)
+            {
+                 if (textBox1.Text[index] == '*')
+                 {
+                     position[pos] = index;
+                     pos++;
+                 }
+            }
+            string finale = textBox1.Text;
+            for (int i = 0; i < Math.Pow(2, starcount); i++)
+            {
+                for (int j = 0; j < starcount; j++)
+                {
+                    string f = starset[i, j].ToString();
+                    finale = finale.Remove(position[j], 1).Insert(position[j], f);
+                }
+                bool result = IsMonotonus(finale, true);
+                if (result) answer.Text += finale + "\n";
+            }
+            if (answer.Text == "") answer.Text = "\nДанная функция\n не может быть \nдоопределена до \nмонотонной";
+
+            calculate.Enabled = false;
+            //int[,] matrix = BoolSet(textBox1.Text.Length);
+            
+            //string l = "01";
+            //bool k = IsMonotonus(l, true);
+        }
+
+        
+
+        private bool IsMonotonus(string vector, bool ok)
+        {
+            if (!ok) return false;
+            int halfsize = vector.Length / 2;
+            // devide a vector into two
+            string right = vector.Substring(0, halfsize);
+            string left = vector.Substring(halfsize, vector.Length - halfsize);
+
+            if (right.Length > 1 && left.Length > 1)
+            {
+                ok = IsMonotonus(right, ok); if (!ok) return false;
+                for (int i = 0; i < halfsize; i++)
+                {
+                    if (Convert.ToInt16(right[i]) <= Convert.ToInt16(left[i])) ok = true; else return false;
+                }
+                ok = IsMonotonus(left, ok); if (!ok) return false;
+            }
+            else
+            {
+                int sd = Convert.ToInt16(right);
+                int a = Convert.ToInt16(left);
+
+                if (Convert.ToInt16(right) > Convert.ToInt16(left)) return false;
+            }
+            return ok;
+
+        }
+
+        private int[,] BoolSet (int length) // матрица, заполненная наборами значений булевой функции
+        {
+            int degree = (int)Math.Log(length, 2);
+            matrix = new int[length, degree];
+            int x; // a number of elements in each group
+            for (int i = 0; i < degree; i++) // cycle filling the matrix of meanings for bool function
+            {
+                int j = 0;
+                bool boolmean = false; // false - 0, true - 1
+                x = Convert.ToInt32(length / Math.Pow(2, i + 1));
+                while (j < length)
+                {
+                    for (int k = j; k < j + x; k++)
+                    {
+                        if (boolmean) matrix[k, i] = 1; else matrix[k, i] = 0;
+                    }
+                    boolmean = !boolmean;
+                    j += x;
+                }
+            }
+            return matrix;
+        }
+    }
+}
